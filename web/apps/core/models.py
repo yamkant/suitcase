d88a4b0ce6148a996854a3b7ffcc2569e49b2aee
yamkant <dev.yamkim@gmail.com>
2023-08-19 18:25:07 +0900

    feat: #9 [App: products] 제품 관련 앱 및 모델 추가

--- web/apps/core/models.py
+++ web/apps/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
---
+++ web/apps/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from core.models import BaseModel
+from users.models import User
+
+class Product(BaseModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=False)
+    image_url = models.URLField(max_length=255)
+    is_favorite = models.CharField(default="N", max_length=1)
+    is_profile = models.CharField(default="N", max_length=1)
+
+    user_id = models.ForeignKey(User, db_column="user_id", on_delete=models.PROTECT)
+
+    class Meta:
+        managed = True
+        db_table = 'products'
