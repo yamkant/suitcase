3532c2c2a19e7267819455d36e9a87520d2c9b53
yamkant <dev.yamkim@gmail.com>
2023-08-20 22:58:01 +0900

    feat: #11 [App: products] 제품 관리 페이지에서 등록 제품 리스트 조회 기능 추가

--- web/apps/client/views.py
+++ web/apps/client/views.py
@@ -4,10 +4,12 @@ from client.libs.custom_decorators import login_required
 
 from products.models import Product
 
+# TODO: 기본 필터는 묶어서 전처리로 수행되도록 수정
+
 @require_http_methods(['GET'])
 @login_required
 def render_home(request):
-    prod_list = Product.objects.filter()
+    prod_list = Product.objects.filter(user_id=request.user.id, is_deleted="N")
     # TODO: Enum to dictionary로 리팩토링
     cate_dict = {
         "UNDEFINED": 1,
@@ -29,3 +31,22 @@ def render_home(request):
     }
 
     return render(request, 'client/home.html', context)
+
+@require_http_methods(['GET'])
+@login_required
+def render_manage(request):
+    prod_list = Product.objects.filter(user_id=request.user.id, is_deleted="N")
+
+    cate_disp_dict = {
+        "UNDEFINED": 1,
+        "PANTS": 2,
+        "TOPS": 3,
+        "SHOES": 4,
+    }
+
+    context = {
+        'prod_list': prod_list,
+        'cate_disp_dict': cate_disp_dict,
+    }
+
+    return render(request, 'client/manage.html', context)
