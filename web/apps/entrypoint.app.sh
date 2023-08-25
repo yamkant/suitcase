35d1b8771950791ee9e50feca9a822a812837cd2
yamkant <dev.yamkim@gmail.com>
2023-08-25 23:41:09 +0900

    feat: #22 [App: client] 로그인 되어있지 않은 상태에서 리다이렉션 수행

--- web/apps/entrypoint.app.sh
+++ web/apps/entrypoint.app.sh
@@ -5,6 +5,5 @@ pip install -r requirements.txt
 
 python3 manage.py migrate
 
-# gunicorn --bind 0.0.0.0:8000 config.wsgi:application
-python3 /apps/manage.py runserver 0.0.0.0:8000
-
+# python3 /apps/manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application
