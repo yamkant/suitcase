6c64ebf0e65fc7291aadc36e2ab02725753c0d83
yamkant <dev.yamkim@gmail.com>
2023-09-12 14:06:54 +0900

    feat: #45 [App: products] 비동기 작업시간 표시 데코레이터 추가

--- web/apps/common/management/commands/img_url_to_file.py
+++ web/apps/common/management/commands/img_url_to_file.py
@@ -3,6 +3,10 @@ from django.core.management.base import BaseCommand
 from core.classes import S3ImageUploader, ImageHandler
 from products.tasks import upload_image_by_image_url
 
+from products.tasks import (
+    upload_image_by_image_url,
+)
+
 # TODO: log 추가
 import logging
 
@@ -18,4 +22,4 @@ class Command(BaseCommand):
         # img_url = "https://us.lemaire.fr/cdn/shop/files/PA326_LD1001_BR495_PS1_2000x.jpeg?v=1690202108"
         img_url = "https://noclaim.co.kr/web/product/extra/small/202211/d0e03850b5704618babeef3269d22cd1.jpg"
 
-        upload_image_by_image_url(img_url, 'tester')
+        upload_image_by_image_url.delay(img_url, 'tester')
