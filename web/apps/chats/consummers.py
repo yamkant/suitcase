6614c0372292d331e192888194fd9b5f3f184c64
yamkant <dev.yamkim@gmail.com>
2023-09-13 11:28:01 +0900

    feat: #45 [App: chats] Socket 통신 관련 앱 및 설정 추가

---
+++ web/apps/chats/consummers.py
@@ -0,0 +1,19 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
