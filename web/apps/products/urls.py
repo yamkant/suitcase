06914ab87007c2cb2eca477c2279e6eeb75057dd
yamkant <dev.yamkim@gmail.com>
2023-11-07 13:27:35 +0900

    feat: #65 [APP: products] ModelViewSet을 GenericAPIView로 변경

--- web/apps/products/urls.py
+++ web/apps/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from products.views.products import ProductViewSet, ProductBulkViewSet
+from products.views.products import ProductBulkViewSet, ProductListView, ProductDetailView
 from products.views.profiles import ProductProfileViewSet
 from django.contrib.auth import views as auth_views
 from core.types import APIUrlPatternsType
@@ -7,8 +7,9 @@ from core.types import APIUrlPatternsType
 app_name = "products"
 
 urlpatterns: APIUrlPatternsType = [
-    path("", ProductViewSet.as_view({"get": "list", "post": "create"}), name="list"),
-    path("<int:id>/", ProductViewSet.as_view({"patch": "update", "delete": "destroy"}), name="detail"),
-    path("bulk/", ProductBulkViewSet.as_view({"patch": "bulk_update", "delete": "bulk_delete"}), name="bulk"),
+    path("", ProductListView.as_view(), name="list"),
+    path("<int:id>/", ProductDetailView.as_view(), name="detail"),
+
+    path("bulk/", ProductBulkViewSet.as_view(), name="bulk"),
     path("profile/", ProductProfileViewSet.as_view({"post": "create"}), name="profile_list"),
 ]
--- web/apps/products/views/products.py
+++ web/apps/products/views/products.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework.response import Response
 from rest_framework import status
 from products.models import Product
@@ -22,8 +25,7 @@ from products.swagger import (
     PRODUCT_UPDATE_EXAMPLES,
 )
 
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
-from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from django.core.cache import cache
 from client.libs.cache import get_cache_product_count_key
 
@@ -32,33 +34,8 @@ from products.tasks import (
 )
 from django_eventstream import send_event
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.active_objects.order_by('-created_at')
-    serializer_class = ProductSerializer
-    permission_classes = [IsOwner]
-    lookup_field = "id"
-    filter_backends = [filters.SearchFilter, ]
-    search_fields = ['name', ]
-    pagination_class = ProductPagination
-
-    serializer_action_classes = {
-        'list': ProductSerializer,
-        'create': ProductCreateSerializer,
-        'update': ProductUpdateSerializer,
-        'destroy': ProductDeleteSerializer,
-    }
-
-    def get_queryset(self):
-        return self.queryset.filter(user_id=self.request.user.id)
-
-    def get_serializer_class(self):
-        try:
-            return self.serializer_action_classes[self.action]
-        except (KeyError, AttributeError):
-            return super().get_serializer_class
-
-    # TODO: 테스터 추가 예정
-    @extend_schema(
+@extend_schema_view(
+    get=extend_schema(
         request=ProductSerializer,
         summary="상품 목록을 조회합니다.",
         description="""상품 목록을 페이지번호/페이지크기/검색결과에 따라 조회합니다.""",
@@ -68,11 +45,8 @@ class ProductViewSet(viewsets.ModelViewSet):
             status.HTTP_200_OK: ProductSerializer,
             status.HTTP_403_FORBIDDEN: None
         }
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-    
-    @extend_schema(
+    ),
+    post=extend_schema(
         request=ProductCreateSerializer,
         summary="새로운 상품을 추가합니다.",
         description="""'Add product' modal에서 상품을 등록하는 예시입니다. 상품의 이미지 주소와 이름을 입력하면 비동기작업으로 업로드가 수행됩니다.""",
@@ -83,6 +57,29 @@ class ProductViewSet(viewsets.ModelViewSet):
             status.HTTP_403_FORBIDDEN: None
         }
     )
+)
+class ProductListView(ListCreateAPIView):
+    queryset = Product.active_objects.order_by('-created_at')
+    serializer_class = ProductSerializer
+    permission_classes = [IsOwner]
+    filter_backends = [filters.SearchFilter, ]
+    search_fields = ['name', ]
+    pagination_class = ProductPagination
+
+    serializer_action_classes = {
+        'GET': ProductSerializer,
+        'POST': ProductCreateSerializer,
+    }
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id=self.request.user.id)
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.request.method]
+        except (KeyError, AttributeError):
+            return super().get_serializer_class
+
     def create(self, request, *args, **kwargs):
         if not request.data.get('image_url'):
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -103,7 +100,8 @@ class ProductViewSet(viewsets.ModelViewSet):
         cache.delete(get_cache_product_count_key(request.user.id))
         return super().create(request, *args, **kwargs)
 
-    @extend_schema(
+@extend_schema_view(
+    patch=extend_schema(
         request=ProductUpdateSerializer,
         summary="상품을 활성화/비활성화 및 정보들을 수정합니다.",
         description="""'Edit product' modal에서 상품의 이름, 카테고리와 같은 기본적인 정보를 수정합니다.<br>is_active 필드의 활성여부에 따라 Fitting 페이지에서 상품을 사용여부가 결정됩니다.""",
@@ -113,12 +111,8 @@ class ProductViewSet(viewsets.ModelViewSet):
             status.HTTP_200_OK: ProductSerializer,
             status.HTTP_403_FORBIDDEN: None
         }
-    )
-    def update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return super().update(request, *args, **kwargs)
-    
-    @extend_schema(
+    ),
+    delete=extend_schema(
         request=ProductUpdateSerializer,
         summary="상품을 제거(논리적 제거)합니다.",
         tags=['상품'],
@@ -129,16 +123,34 @@ class ProductViewSet(viewsets.ModelViewSet):
             status.HTTP_403_FORBIDDEN: None
         }
     )
-    def destroy(self, request, *args, **kwargs):
+)
+class ProductDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.active_objects.order_by('-created_at')
+    lookup_field = "id"
+    serializer_action_classes = {
+        'PATCH': ProductUpdateSerializer,
+        'DELETE': ProductDeleteSerializer,
+    }
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id=self.request.user.id)
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.request.method]
+        except (KeyError, AttributeError):
+            return super().get_serializer_class
+
+    def delete(self, request, *args, **kwargs):
         cache.delete(get_cache_product_count_key(request.user.id))
         return super().update(request, *args, **kwargs)
 
-class ProductBulkViewSet(viewsets.ModelViewSet):
+class ProductBulkViewSet(RetrieveUpdateDestroyAPIView):
     queryset = Product.active_objects
     serializer_class = ProductSerializer
     permission_classes = [IsOwner]
 
-    def bulk_update(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         if 'prod_list' not in request.data:
             return Response({})
         prod_id_list = request.data.get('prod_list')
@@ -151,7 +163,7 @@ class ProductBulkViewSet(viewsets.ModelViewSet):
         send_event(request.user.username, 'message', {"type": f"edit"})
         return Response({})
 
-    def bulk_delete(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         if 'prod_list' not in request.data:
             return Response({})
         prod_id_list = request.data.get('prod_list')
